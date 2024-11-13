5cf1917ec5e58286af30980c5c95b2cf500aa90e
alfcxgh1 <98395551+alfcxgh1@users.noreply.github.com>
2024-11-13 10:02:16 +0300

    Add files via upload

--- js1/abc.js
+++ js1/abc.js
@@ -1,1 +1,22 @@
+function verify(a,b,c) {
+        console.log("a, b, c")
+    let a = parseInt(elementA.innerText);
+    let b = parseInt(elementB.innerText);
+    let c = parseInt(elementC.innerText);
 
+    if (a<b && b<c){
+        result='Выполняется неравенство  A < B < C';
+    }
+    else if (a<b && b>c){
+        result = "Выполняется неравенство  A < B > C";
+    }
+    else
+        result='Не выполняется'
+    }
+
+document.getElementById("result").innerText = result;
+
+}
+
+const elementVerify = document.getElementById("verify");
+elementVerify.addEventListener('click', verify);
